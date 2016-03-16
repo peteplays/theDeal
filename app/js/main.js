a23d49626d52e5691b9854d973e656261af74142
Peter Schnabel <pschnabel@carekinesis.com>
2016-03-16 16:08:24 -0400

    updated file structure and added angular-ui-bootstrap

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
+require('angular/angular.min');
+require('angular-animate/angular-animate.min');
+require('angular-ui-bootstrap/dist/ui-bootstrap-tpls');
+
 console.log('this is awesome!!!!!');
 
 var hi = document.getElementById('helloFromJs');
-hi.innerHTML += '<em>Hello</em> from <code>main.js</code>!!!';
+hi.innerHTML += '<em>Hello...</em> from <kbd>app/js/</kbd> <code>main.js</code>!!!';
+
+var theApp = angular.module('theApp', ['ui.bootstrap', 'ngAnimate'])
+    .controller('template', function($scope){
+        $scope.hello = 'Hello From Angular';
+        $scope.isCollapsed = true;
+    });
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 var port = '5555';
 
 console.log('Server running at http://localhost:'+port);
 
-app.use(express.static('prod'));
 app.use(express.static('media'));
+app.use(express.static('www'));
+
 
 app.get('/',function(req,res){
-    res.sendFile('/index.html');
+    res.sendFile('index');
+    //res.sendFile(path.join(__dirname + '/www/indexp.html'));
 });
 
 app.listen(port);
